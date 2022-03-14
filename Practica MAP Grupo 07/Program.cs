f01bedba277c041fcabd27098babc304885ce483
Pablo Sanchez <pablos19@ucm.es>
2022-03-14 11:40:20 +0100

    Archivos inciales añadidos al índice del repositorio

---
+++ Practica MAP Grupo 07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_MAP_Grupo_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
